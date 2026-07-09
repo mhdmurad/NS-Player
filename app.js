74ea521782decb556f1551bcee83e0cfca45fabb
mhdmurad <139557905+mhdmurad@users.noreply.github.com>
2026-07-09 12:41:10 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,61 +1,27 @@
-const container = document.getElementById("channels");
-const search = document.getElementById("search");
+const API =
+"https://raw.githubusercontent.com/mhdmurad78/NS-Player/main/toffee_data.json";
 
-const WORKER = "https://young-river-6627.mhdmurad78.workers.dev";
-
-let channels = [];
-
-fetch("https://raw.githubusercontent.com/sm-monirulislam/Toffee-Auto-Update-Playlist/main/toffee_data.json")
-.then(res => res.json())
+fetch(API)
+.then(r => r.json())
 .then(data => {
 
-    channels = data.response;
+    const box = document.getElementById("channels");
 
-    showChannels(channels);
+    data.response.forEach((ch,index)=>{
 
-});
-
-function showChannels(list){
-
-    container.innerHTML = "";
-
-    list.forEach(ch=>{
-
-        const index = channels.indexOf(ch);
-
-        container.innerHTML += `
-
+        box.innerHTML += `
         <div class="card" onclick="play(${index})">
-
-            <img src="${ch.logo}" loading="lazy">
-
+            <img src="${ch.logo}">
             <h3>${ch.name}</h3>
-
         </div>
-
         `;
 
     });
-
-}
-
-search.addEventListener("keyup",()=>{
-
-    const value = search.value.toLowerCase();
-
-    const result = channels.filter(item=>
-
-        item.name.toLowerCase().includes(value)
-
-    );
-
-    showChannels(result);
 
 });
 
 function play(id){
 
-    window.location =
-    "player.html?id=" + id;
+location.href="player.html?id="+id;
 
 }
